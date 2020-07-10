cb6fd3a81dd8ec076e761d029b48002744b641d3
Papama Bacela <pabacela@c4r21s3.wethinkcode.co.za>
2020-07-10 15:07:01 +0200

    Second Commit :Function that takes a pointer x9

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,33 @@
+#include<unistd.h>
+ 
+
+void ft_ultimate_ft(int *********nbr)
+{
+  		int j = 42;
+		int *c1;
+		int **c2;
+		int ***c3;
+		int ****c4;
+		int *****c5;
+		int ******c6;
+		int *******c7;
+		int ********c8;
+		
+				
+
+
+
+
+		c1 = &j;
+		c2 = &c1;
+		c3 = &c2;
+		c4 = &c3;
+		c5 = &c4;
+		c6 = &c5;
+		c7 = &c6;
+	    c8 = &c7;
+
+		nbr = &c8;	
+
+	 	
+}	
